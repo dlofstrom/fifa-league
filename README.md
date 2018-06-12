9c37e5b2c3ad0be7d4d984f7820eaaf99eb902fc
dlofstrom <lofstrom.daniel@gmail.com>
2018-06-12 15:17:21 +0200

    Table and finals generation working for 5 teams

--- README.md
+++ README.md
@@ -2,12 +2,29 @@
 A FIFA league application using Slack as frontend
 
 ## Setup
-- Setup [ngrok](https://ngrok.com/) and run a http tunnel e.g: `ngrok http 3000`
-- Setup a Slack app with two slash commands `/signup` and `/result @hometeam @awayteam result` following (this guide)[https://girliemac.com/blog/2016/10/24/slack-command-bot-nodejs/] with Request URL set to `ngrok-url/api/command-name`
-- Save the Bot User OAuth Access Token in `echo -n TOKEN > bot-token` and channel name as `echo -n CHANNEL > bot-token`
-- Install [Node.js](https://nodejs.org/) and [npm](https://www.npmjs.com/) and run `npm install` and `node server.js`
+- Install [Node.js](https://nodejs.org/) and [npm](https://www.npmjs.com/)
+- Setup [ngrok](https://ngrok.com/) and run a http tunnel e.g: `ngrok http 8000`
+- Run `npm install` and `node server.js`
+- Setup a Slack app and create an event subscription pointing to the https domain provided by ngrok followed by `/api/user`
+- Set the event subscription to post when a user joins and when a user changes information
+- Create a Boy user in the Slack app
+- Save the Bot User OAuth Access Token in root folder `echo -n TOKEN > bot-token`
 
 ## Slack commands
+In the Slack app, setup the following slash commands, all pointing to the https domain provided by ngrok followed by `/api/command-name`
+Admin commands:
+- `/admin` sets current user and channel to admin channel, only works once
+- `/public` sets current channel to the official channel for results, so that private chats wont work
+- `/drop` @team removes team and all results from league
+Public commands:
+- `/rules` displays all commands and league rules
 - `/signup` registers user with slack user handle as team name
 - `/result @hometeam @awayteam result(e.g 3-1)` registers a game result
+- `/games` @team displays available games for team
+- `/table` prints league table
+- `/stats` @team prints more details about a team
 
+## Setup league
+- In your private channel with Slack app write `/admin`
+- In the desired public league channel write `/public`
+- Users can now execute commands and play games
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var fs = require("fs");
-const { WebClient } = require('@slack/client');
+const { WebClient } = require("@slack/client");
+//var localtunnel = require("localtunnel");
 
 var app = express();
 app.use(bodyParser.json())
@@ -9,32 +10,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 var path = __dirname;
 
 var token = fs.readFileSync(path + "/bot-token", "utf8");
-var channel = fs.readFileSync(path + "/bot-channel", "utf8");
 const web = new WebClient(token);
 
-
 /*
-web.chat.postMessage({ channel: channel, text: 'Hello there' })
-  .then((res) => {
-    // `res` contains information about the posted message
-    console.log('Message sent: ', res.ts);
-  })
-  .catch(console.error);
+var subdomain = fs.readFileSync(path + "/subdomain", "utf8");
+console.log("Chosen subdomain " + subdomain);
+var tunnel = localtunnel(8000, {subdomain:subdomain}, function(err, tunnel) {
+    console.log("Tunnel opened on " + tunnel.url);
+});
 */
-
-//Get requests from frontend
-app.get("/", function(req, res) {
-    res.sendFile(path + "/index.html");
-    console.log("/");
-});
-
-app.get("/:any", function(req, res) {
-    var any = req.params.any;
-    res.sendFile(path + "/" + any);
-    console.log(any);
-});
-
-
 
 var getJSON = function() {
     var json = {};
@@ -59,7 +43,15 @@ var getJSON = function() {
     }
     if (!json.hasOwnProperty("users")) {
         console.log("No field named users, creating: " + json);
-        json.users = {};
+        json.users = "";
+    }
+    if (!json.hasOwnProperty("admin")) {
+        console.log("No field named admin, creating: " + json);
+        json.admin = {"user":"", "channel":""};
+    }
+    if (!json.hasOwnProperty("public")) {
+        console.log("No field named public, creating: " + json);
+        json.public = "";
     }
     
     return json;
@@ -143,18 +135,161 @@ var generateTable = function(json) {
     return table;
 }
 
+//Get final bracket order (e.g 1 8 4 5 2 7 3 6)
+var getOrder = function(n,o) {
+    var order = [o]; //order array
+    while (order.length < n) {
+        var indices = order.map(function(e,i){return i}); //0,1,2... same length as order
+        indices = indices.sort(function(i,j){return order[j]-order[i]}); //get indices to sorted order (e.g [4,1,3,2]=>[0,2,3,1])
+        indices.map(function(index,i) {
+            order[index] = [order[index], order.length+i+o];
+        });
+        order = [].concat.apply([],order);
+        //console.log(order);
+    }
+    return order;
+}
+
 var generateBracket = function(json) {
-    //Use table
+    //Number of teams
+    var n = json.table.length;
 
-    //If less than 8
+    //Generate winners bracket team order (1 8 4 5 2 7 3 6)
+    var nw = (n > 8) ? 8 : n;
+    var order = getOrder(nw,0);
+    console.log("Order of winners bracket " + order);
+
+    //Generate static winners bracket
+    json.finals["0"] = {"played":false, "date":0, "teams":{"home":"", "away":""}, "goals":{"home":0, "away":0},
+                        "next":{"game":"done", "team":""}};
+
+    //TODO fix l0 and w0 pointing to 0    
+    var stage = 1;
+    var team = ["away", "home"];
+    var w = 1;
+    while (stage < nw) {
+        for (var i = 1; i <= stage; i++) {
+            json.finals["w"+w] = {"played":false, "date":0, "teams":{"home":"", "away":""}, "goals":{"home":0, "away":0},
+                                  "next":{"game":"w"+Math.floor(w/2), "team":team[w%2]}};
+            w += 1;
+        }
+        stage *= 2;
+    }
+    //console.log("Winners generated");
+    //console.log(json.finals);
+
+
+    //Generate losers bracket
+    //If there are more than 8 players in losers, keep generating games in losers (but minimum 8)
+    nl = (n-8 > 8) ? n-8 : 8;
+    stage = 1;
+    var l = 1;
+    while (stage < nl) {
+        for (var i = 1; i <= stage; i++) {
+            json.finals["l"+l] = {"played":false, "date":0, "teams":{"home":"", "away":""}, "goals":{"home":0, "away":0},
+                                  "next":{"game":"l"+Math.floor(l/2), "team":team[(l+1)%2]}};
+
+            //Generate revenge (r) stages for all losers in winners bracket
+            if (stage <= 4) {
+                //Add r match pointing
+                json.finals["l"+l+"r"] = {"played":false, "date":0, "teams":{"home":"", "away":""}, "goals":{"home":0, "away":0},
+                                          "next":{"game":"l"+Math.floor(l/2), "team":team[(l+1)%2]}};
+
+                //Modify earlier created match to point to revenge match instead
+                json.finals["l"+l].next.game = "l"+l+"r";
+                //Change team to away (loser from wb always home)
+                json.finals["l"+l].next.team = "away";
+            }
+
+            l += 1;
+        }
+        stage *= 2;
+    }
+    console.log("Finals generated");
+    console.log(json.finals);
     
-    //Else if more than 8
     
+    //Fill winners bracket first stage matches from order
+    order.forEach(function(position) {
+        if (position < nw) {
+            //Team exists, add to game (based on w)
+            console.log("Adding team " + position + " to game");
+            json.finals["w"+Math.floor(w/2)].teams[team[w%2]] = json.table[position].name;
+        } else {
+            console.log("Team " + position + " does not exist");
+            //No team, move "winning" team to next
+            json.finals["w"+Math.floor(w/2)].played = true;
+            var next = json.finals["w"+Math.floor(w/2)].next;
+            moveBracket(json, next, json.finals["w"+Math.floor(w/2)].teams[team[(w+1)%2]]);
+            //json.finals[next.game].teams[next.team] = json.finals["w"+Math.floor(w/2)].teams[team[(w+1)%2]];
+            
+            //Also move team in losing bracket
+            //Mark losing as walk over
+            next = {"game":"l"+Math.floor(w/2)+"r", "team":"home"};
+            //json.finals[losing].teams["home"] = "WALKOVER";
+            moveBracket(json, next, "WALKOVER");
+        }
+        w += 1;
+    });
+    //console.log("Winners filled");
+    //console.log(json.finals);
+    
+
+    //Fill losers bracket
+    //Add more teams if more than 8 (those who fit in winners bracket)
+    order = getOrder(nl, 8);
+    //Fill leauge loser stage matches from order
+    order.forEach(function(position) {
+        if (position < n) {
+            //Team exists, add to game (based on l)
+            console.log("Adding team " + position + " to game");
+            json.finals["l"+Math.floor(l/2)].teams[team[l%2]] = json.table[position].name;
+        } else {
+            //Check if other team exists and move it to next stage
+            var other = json.finals["l"+Math.floor(l/2)].teams[team[(l+1)%2]];
+            var next = json.finals["l"+Math.floor(l/2)].next;
+            json.finals["l"+Math.floor(l/2)].played = true;
+            
+            if(other != "") {
+                //move "winning" team to next
+                moveBracket(json, next, other);
+                //json.finals[next.game].teams[next.team] = other;
+            } else {
+                //If no teams, "next" will be walk over
+                moveBracket(json, next, "WALKOVER");
+                //json.finals[next.game].teams[next.team] = "WALKOVER";
+            }
+
+            //TODO handle two stages of walkover (recursion?)
+        }
+        l += 1;
+    });
+    //console.log("Finals filled");
+    //console.log(json.finals);
+
+    return json.finals;
 }
 
-var updateBracket = function(json) {
+
+var moveBracket = function(json, next, team) {
+    var other = (next.team === "home") ? "away" : "home";
+    //if (json.finals[next.game].teams[other] === "WALKOVER") {
+
+    //Set team in next game
+    json.finals[next.game].teams[next.team] = team;
+
+    console.log("Move Bracket:");
+    console.log(next);
+    console.log(team);
     
+    //If the other team left walk over
+    if (json.finals[next.game].teams[other] === "WALKOVER") {
+        json.finals[next.game].played = true;
+        //Keep moving team
+        moveBracket(json, json.finals[next.game].next, team);
+    }
 }
+
 
 var processResult = function(d, json) {
     r = {"valid":true, "ht":"", "at":"", "hg":0, "ag":0}
@@ -198,6 +333,49 @@ var processResult = function(d, json) {
     return r;
 }
 
+
+//Chat posts (into public channel)
+var chatSignup = function(user, json) {
+    //res.json({response_type: "in_channel", text: "\<\@" + u + "\> was added to the league!"}); //echo back
+    web.chat.postMessage({channel: json.public, text: "\<\@" + user + "\> was added to the league!"})
+        .then((res) => {
+            // `res` contains information about the posted message
+            console.log("Message sent to public channel: ", res.ts);
+        })
+        .catch(console.error);
+}
+
+var chatResult = function(result, json, state) {
+    var text = "";
+    //First add match text
+    if (result != "") {
+        if (result.hg > result.ag)
+            text += "\<\@"+result.ht+"\> played \<\@"+result.at+"\> and won ("+result.hg+"-"+result.ag+")";
+        else if (result.hg < result.ag)
+            text += "\<\@"+result.ht+"\> played \<\@"+result.at+"\> and lost ("+result.hg+"-"+result.ag+")";
+        else
+            text += "\<\@"+result.ht+"\> played \<\@"+result.at+"\> and the result was a draw ("+result.hg+"-"+result.ag+")";
+    }
+
+    //If league, add league table
+    text += "\n\n*Current league table:*"
+    json.table.map(function(r) {
+        text += "\n"+r.pts+" \<\@"+r.name+"\> ("+r.gd+", "+r.f+")";
+    });
+    
+    //If finals, add finals brackets
+
+    //Print chat
+    web.chat.postMessage({channel: json.public, text: text})
+        .then((res) => {
+            // `res` contains information about the posted message
+            console.log("Message sent to public channel: ", res.ts);
+        })
+        .catch(console.error);
+}
+
+
+
 //Handle data post
 app.post("/api/:type", function(req, res) {
     var type = req.params.type;
@@ -229,7 +407,9 @@ app.post("/api/:type", function(req, res) {
             console.log(json);
             
             //Respond with successful add to league
-            res.json({response_type: "in_channel", text: "\<\@" + u + "\> was added to the league!"}); //echo back
+            //res.json({response_type: "in_channel", text: "\<\@" + u + "\> was added to the league!"}); //echo back
+            chatSignup(u, json);
+            res.send("Team \<\@" + u + "\> signed up!");
         } else {
             //If team already is signed up
             res.send("Team \<\@" + u + "\> already signed up");
@@ -255,13 +435,12 @@ app.post("/api/:type", function(req, res) {
                     json.league[result.ht + "-" + result.at].goals.away = parseInt(result.ag);
                     json.league[result.ht + "-" + result.at].date = Date.now();
 
+                    //Generate league table
+                    json.table = generateTable(json);
+                    
                     //Channel response
-                    if (result.hg > result.ag)
-                        res.json({response_type:"in_channel", text:"\<\@"+result.ht+"\> played \<\@"+result.at+"\> and won ("+result.hg+"-"+result.ag+")"});
-                    else if (result.hg < result.ag)
-                        res.json({response_type:"in_channel", text:"\<\@"+result.ht+"\> played \<\@"+result.at+"\> and lost ("+result.hg+"-"+result.ag+")"});
-                    else
-                        res.json({response_type:"in_channel", text:"\<\@"+result.ht+"\> played \<\@"+result.at+"\> and the result was a draw ("+result.hg+"-"+result.ag+")"});
+                    chatResult(result, json, true);
+                    res.send("Result \<\@"+result.ht+"\>-\<\@"+result.at+"\> ("+result.hg+"-"+result.ag+") recorded");
                 }
                 //Else check return game
                 else if (json.league[result.at + "-" + result.ht].played === false) {
@@ -271,27 +450,23 @@ app.post("/api/:type", function(req, res) {
                     json.league[result.at + "-" + result.ht].goals.away = parseInt(result.hg);
                     json.league[result.at + "-" + result.ht].date = Date.now();
 
+                    //Generate league table
+                    json.table = generateTable(json);
+
                     //Channel response
-                    if (result.hg > result.ag)
-                        res.json({response_type:"in_channel", text:"\<\@"+result.ht+"\> played \<\@"+result.at+"\> and won ("+result.hg+"-"+result.ag+")"});
-                    else if (result.hg < result.ag)
-                        res.json({response_type:"in_channel", text:"\<\@"+result.ht+"\> played \<\@"+result.at+"\> and lost ("+result.hg+"-"+result.ag+")"});
-                    else
-                        res.json({response_type:"in_channel", text:"\<\@"+result.ht+"\> played \<\@"+result.at+"\> and the result was a draw ("+result.hg+"-"+result.ag+")"});
+                    chatResult(result, json, true);
+                    res.send("Result \<\@"+result.at+"\>-\<\@"+result.ht+"\> ("+result.ag+"-"+result.hg+") recorded");
                 }
                 //Else the teams should not meet
                 else {
                     res.send("Teams have already met twice");
                 }
-
-                //Generate league table
-                json.table = generateTable(json);
                 
                 //Count again if there are any league games left
                 if (Object.values(json.league).filter(function(m){return !m.played}).length === 0) {
                     //If not, generate final brackets
                     console.log("GENERATE FINAL BRACKETS");
-                    generateBracket(json);
+                    json.finals = generateBracket(json);
                 }
             } else {
                 //FINALS GAME
@@ -322,13 +497,75 @@ app.post("/api/:type", function(req, res) {
 
     //Print table (private)
     else if (type === "table") {
-        text = "*League table:*\nPos Team GP W D L F A GD Pts";
+        text = "*League table:*";
         json.table.map(function(r) {
-            text += "\n"+r.pos+" \<\@"+r.name+"\> "+r.gp+" "+r.w+" "+r.d+" "+r.l+" "+r.f+" "+r.a+" "+r.gd+" "+r.pts;
+            text += "\n"+r.pts+" \<\@"+r.name+"\> ("+r.gd+", "+r.f+")";
         });
         res.send(text);
     }
 
+
+    //Print help and rules
+    else if (type === "rules") {
+        res.send("TODO");
+    }
+
+    //Print detailed stats
+    else if (type === "stats") {
+        res.send("TODO");
+    }
+
+
+    //Set channel and user as admin
+    else if (type === "admin") {
+        if (json.admin.user === "") {
+
+            if (json.users.length === 0) {
+                web.users.list()
+                    .then((res) => {
+                        // `res` contains information about the posted message
+                        //console.log('Message sent: ', res);
+                        json.users = {};
+                        res.members.map(function(m) {
+                            json.users[m.name] = {"user_id":m.id, "user_name":m.name, "real_name":m.real_name, "display_name":m.profile.display_name};
+                        });
+                        console.log(json.users);
+                        json.admin.user = json.users[entry.user_name].user_id;
+                        json.admin.channel = entry.channel_id;
+                        writeJSON(json);
+                    })
+                    .catch(console.error);
+                res.send("You are now the admin on this channel");
+            } else {
+                json.admin.user = json.users[entry.user_name].user_id;
+                json.admin.channel = entry.channel_id;
+                writeJSON(json);
+                res.send("You are now the admin on this channel");
+            }
+        } else {
+            res.send("Someone else is admin");
+        }
+    }
+
+    //Set channel as public
+    else if (type === "public") {
+        if (json.admin.user === json.users[entry.user_name].user_id) {
+            json.public = entry.channel_id;
+            writeJSON(json);
+            res.send("This channel is now the public channel");
+        } else {
+            res.send("Someone else is admin");
+        }
+    }
+
+    //Remove team
+    else if (type === "drop") {
+        res.send("TODO");
+
+    }
+
+
+    
     //Event subscription when a user changed information or someone joined the channel
     else if (type === "user") {
         res.json({"challenge":entry.challenge});
@@ -345,8 +582,6 @@ app.post("/api/:type", function(req, res) {
             })
             .catch(console.error);
     }
-
-
     
     else {
         res.send("API function does not exist");
