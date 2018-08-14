610a66b430ffdb2220631b1254ea9fdb8a2aa670
dlofstrom <lofstrom.daniel@gmail.com>
2018-08-14 08:50:01 +0200

    games command bug fix

--- server.js
+++ server.js
@@ -743,7 +743,9 @@ app.post("/api/:type", function(req, res) {
         //Filter non played games in either league or finals
         var games = Object.values(json.league).filter(function(m){return !m.played});
         //If all leagu games are played use finals instead
-        if (games.length === 0) games = Object.values(json.finals).filter(function(m){return !m.played});
+        if (games.length === 0) games = Object.values(json.finals).filter(function(m){
+            return !m.played && m.teams.home != "" && m.teams.home != "WALKOVER" && m.teams.away != "" && m.teams.away != "WALKOVER");
+        });
 
         //Filter user games if specified
         if (u != "") {
